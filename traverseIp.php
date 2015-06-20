3972c4ba096a6dcc2264e716174093b79da6e4b2
sanfen <a@sanfen.me>
2015-06-20 15:44:02 +0800

    replace tab to space

--- traverseIp.php
+++ traverseIp.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Author	: sanfen
- * Time		: 2015-06-20
- * Email	: a@sanfen.me
+ * Author   : sanfen
+ * Time     : 2015-06-20
+ * Email    : a@sanfen.me
  * 输入起始ip和结束ip，中间遍历操作。
  */
 
@@ -11,14 +11,14 @@
  */
 function ip2Num($ipStr)
 {
-	$ipNum = 0;
-	$ipArray = explode(".", $ipStr);
-	
-	for($i=0; $i<4; $i++) {
-		$ipNum += $ipArray[$i]*pow(255, 3-$i);
-	}
-	
-	return $ipNum;
+    $ipNum = 0;
+    $ipArray = explode(".", $ipStr);
+    
+    for($i=0; $i<4; $i++) {
+        $ipNum += $ipArray[$i]*pow(255, 3-$i);
+    }
+    
+    return $ipNum;
 }
 
 
@@ -27,31 +27,31 @@ function ip2Num($ipStr)
  */
 function num2Ip($ipNum)
 {
-	$ipArray = array();
-	
-	for($i=0; $i<4; $i++) {
-		$div = pow(255, 3-$i);
-		$ipArray[$i] = intval( $ipNum/$div );
-		$ipNum = $ipNum % $div;
-	}
-	return implode(".", $ipArray);
+    $ipArray = array();
+    
+    for($i=0; $i<4; $i++) {
+        $div = pow(255, 3-$i);
+        $ipArray[$i] = intval( $ipNum/$div );
+        $ipNum = $ipNum % $div;
+    }
+    return implode(".", $ipArray);
 }
 
 
 function traverseIp($startIp, $endIp, $callback)
 {
-	$ipStartNum = ip2Num($startIp);
-	$ipEndNum = ip2Num($endIp);
-	
-	//ip检查 排序
-	if( $ipStartNum > $ipEndNum) {
-		list($ipStartNum, $ipEndNum) = array($ipEndNum, $ipStartNum);
-	}
-	
-	for( $ip=$ipStartNum; $ip<=$ipEndNum; $ip++) {
-		
-		$callback(num2Ip($ip));
-	}
+    $ipStartNum = ip2Num($startIp);
+    $ipEndNum = ip2Num($endIp);
+    
+    //ip检查 排序
+    if( $ipStartNum > $ipEndNum) {
+        list($ipStartNum, $ipEndNum) = array($ipEndNum, $ipStartNum);
+    }
+    
+    for( $ip=$ipStartNum; $ip<=$ipEndNum; $ip++) {
+        
+        $callback(num2Ip($ip));
+    }
 }
 
 
